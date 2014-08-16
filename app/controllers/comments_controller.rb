1efc42634e04a5982b19c8a09102ee596a0e96d2
gond0026@umn.edu <gond0026@umn.edu>
2014-08-16 16:49:50 -0500

    added comment and post controllers not finished

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+  include ApplicationHelper
+  def create
+    current_user
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create(comment_params)
+    @comment.update(commenter: @current_user)
+    redirect_to user_path(Memorial.find(@post.memorial_id))
+  end
+
+  def new
+    @comment = Comment.new
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:commenter, :text)
+    end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,29 @@
+include PostsHelper
+
 class PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    current_user
+    if @current_user
+      new_post = Post.create(post_params)
+      new_post.save
+      redirect_to post_path(Memorial.find(new_post.post_id))
+    else
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:text)
+    end
+
 end
